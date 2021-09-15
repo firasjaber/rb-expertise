3320e7283ebcd3b19360046159b24404d19522f6
Firas Jaber <firasjaberc@gmail.com>
2021-09-15 21:10:01 +0100

    moved the generated files

--- backend/generated/nexus-typegen.ts
+++ backend/generated/nexus-typegen.ts
@@ -4,7 +4,7 @@
  */
 
 
-import type { Context } from "./src/context"
+import type { Context } from "./../src/context"
 
 
 declare global {
--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -6,8 +6,8 @@ import * as types from './graphql';
 export const schema = makeSchema({
   types,
   outputs: {
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
-    schema: join(__dirname, '..', 'schema.graphql'),
+    typegen: join(__dirname, '..', 'generated', 'nexus-typegen.ts'),
+    schema: join(__dirname, '..', 'generated', 'schema.graphql'),
   },
   contextType: {
     module: join(__dirname, './context.ts'),
