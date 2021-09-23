514fc8783f041e566b89e2965de021f645b686db
Firas Jaber <firasjaberc@gmail.com>
2021-09-23 17:02:27 +0100

    employee tests boilerplate

--- backend/__tests__/int/Employee.int.test.ts
+++ backend/__tests__/int/Employee.int.test.ts
@@ -1,15 +1,29 @@
 import { ApolloServer, gql } from 'apollo-server-express';
 import { context } from '../../src/context';
 import { apolloServerConfig } from '../../src/server';
-import { createTestContext } from '../config/__helpers';
 
-const TEST_QUERY = gql`
-  query Query {
-    me
+const GET_ALL_EMPLOYEES = gql`
+  query getAllEmployeesQuery {
+    employees {
+      firstName
+      id
+      lastName
+      pictureUrl
+      email
+      isAdmin
+      phone
+      birthDate
+      address
+      city
+      endDate
+      startDate
+      region
+      createdAt
+    }
   }
 `;
 
-describe('tests', () => {
+describe('Employee tests', () => {
   let server: ApolloServer;
   const typename = 'Employee';
   server = new ApolloServer(apolloServerConfig);
@@ -18,16 +32,41 @@ describe('tests', () => {
     //context.prisma.employee.deleteMany();
     await context.prisma.$disconnect();
   });
-
-  it('testing test', async () => {
-    const mockHelloWorld = {
-      me: 'hello world',
-    };
+  it('get all employees query should return empty when the db is emplty', async () => {
     const res = await server.executeOperation({
-      query: TEST_QUERY,
+      query: GET_ALL_EMPLOYEES,
     });
     expect(res.data).toBeDefined();
-    expect(res.data?.me).toBeDefined();
-    expect(res.data?.me).toBe(mockHelloWorld.me);
+    expect(res.data?.employees).toStrictEqual([]);
+  });
+  it('create employee mutation: invalid input', async () => {
+    expect(true).toBe(false);
+  });
+  it('create employee mutation: already exists email', async () => {
+    expect(true).toBe(false);
+  });
+  it('create employee mutation: valid input', async () => {
+    expect(true).toBe(false);
+  });
+  it('edit employee mutation: invalid input', async () => {
+    expect(true).toBe(false);
+  });
+  it('edit employee mutation: not existing employee', async () => {
+    expect(true).toBe(false);
+  });
+  it('edit employee mutation: valid input', async () => {
+    expect(true).toBe(false);
+  });
+  it('delete employee mutation: invalid input', async () => {
+    expect(true).toBe(false);
+  });
+  it('delete employee mutation: not existing employee input', async () => {
+    expect(true).toBe(false);
+  });
+  it('delete employee mutation: valid input', async () => {
+    expect(true).toBe(false);
+  });
+  it('get all employees query return array list', async () => {
+    expect(true).toBe(false);
   });
 });
