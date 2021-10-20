700aca8491e40fe3dbbb17ab2f8966de48d12f24
Firas Jaber <firasjaberc@gmail.com>
2021-10-20 23:34:26 +0100

    small changes

--- frontend/src/pages/Missions/Missions.tsx
+++ frontend/src/pages/Missions/Missions.tsx
@@ -7,7 +7,6 @@ interface Props {}
 
 const Missions = (props: Props) => {
   const { loading, data, error } = useGetMissionsQuery();
-  console.log(data);
   if (error) return <p>Error loading data...</p>;
   return (
     <div className='flex flex-col flex-1 p-10 px-14 font-nunito'>
