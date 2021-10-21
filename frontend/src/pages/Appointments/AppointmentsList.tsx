3a713abc7a7055d5e7cb7168bd06b9463e533234
Firas Jaber <firasjaberc@gmail.com>
2021-10-21 21:10:27 +0100

    minor changes

--- frontend/src/pages/Appointments/AppointmentsList.tsx
+++ frontend/src/pages/Appointments/AppointmentsList.tsx
@@ -7,7 +7,6 @@ interface Props {}
 
 const AppointmentsList = (props: Props) => {
   const { data } = useGetAppointmentsQuery();
-  console.log(data);
   return (
     <div className='flex flex-col p-4 py-6 space-y-6'>
       {data?.appointments?.map((appData: any) => (
